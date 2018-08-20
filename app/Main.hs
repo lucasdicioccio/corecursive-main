2c7292cbf8ce02a7539d9fe67d7b6530e24eecc3
Lucas DiCioccio <lucas@dicioccio.fr>
2018-08-20 22:49:38 +0200

    Add recursive job split example.
    This is a properly-recursive split where we decide to recurse based on
    the size of the input.
    
    This example is terribly far from a real use case. However, I think this
    example shows well that there is a pattern of looking at the input, then
    splitting the input or not depending on the size.

--- app/Main.hs
+++ app/Main.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE TypeApplications #-}
 module Main where
 
 import Control.Concurrent.Async
@@ -9,6 +10,7 @@ import System.Posix.Process
 data Arg = 
     Help
   | Things [String]
+  | Job Int
   | SSHLeader String [String] -- User [Host]
 
 main :: IO ()
@@ -18,16 +20,31 @@ main = runCorecursiveApp (app parse unparse go)
     parse []                  = pure Help
     parse ("rec":xs)          = pure $ Things xs
     parse ("ssh-leader":x:xs) = pure $ SSHLeader x xs
+    parse ("job":x:[])        = pure $ Job (read x)
     parse args                =
         putStrLn ("un-understood arguments: " ++ show args) >> pure Help
 
     unparse :: Arg -> IO [String]
     unparse Help        = pure []
     unparse (Things xs) = pure ("rec":xs)
+    unparse (Job n)     = pure ("job":show n:[])
 
     go _ Help                    = printHelp
     go self arg@(Things xs)      = goThings self xs
     go self arg@(SSHLeader u hs) = goSSHLeader self u hs
+    go self arg@(Job n)          = goBigJob self n
+
+goBigJob self n
+    -- If n is large, split the input in two and ask two jobs to work on
+    -- smaller inputs each job may do so recursively. This is possible because
+    -- multiplication distributes over multiplication.
+    | n > 1000   = do
+        let (j1, j2) = splitBigJob n
+        [r1, r2] <- mapConcurrently (\n -> readCallback self (Job n)) [j1, j2]
+        putStrLn (show $ read @Int r1 + read @Int r2)
+    | otherwise = putStrLn (show $ n * 2)
+  where
+    splitBigJob n = let k = n `div` 2 in (k, n - k)
 
 goSSHLeader self user hosts = do
     -- Prepare to copy in homedirs with an index so that each copy gets its own
@@ -57,6 +74,7 @@ goThings self xs
 
 printHelp = putStrLn $ unlines [
     "usage: "
-  , "<exe> rec   # print ten recursive things"
+  , "<exe> rec                             # print ten recursive things"
   , "<exe> ssh-leader <user> <hostname|..> # copy as user on hosts and call 'rec' on each of them concurrently"
+  , "<exe> job <n>                         # double a number by recursively breaking it in small pieces"
   ]
